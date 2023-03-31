9fb64a8bcb4097e174f30dcbcca74fa212088682
AnnaChakhvashvili <chaxvashvili12@gmail.com>
2023-03-31 09:07:39 -0700

    first commit

---
+++ Day2/lesson2.py
@@ -0,0 +1,15 @@
+name="Ana"
+surname="chakhvashvili"
+age=21
+height=168.5
+is_beautiful=True
+is_evil=False
+good_bye=()
+print(type(good_bye))
+
+print(name +" "+ surname + " " + str(age) + " " +  str(height) + " " + str(is_beautiful) + " " + str(is_evil) + " " + str(good_bye))
+
+
+
+
+
